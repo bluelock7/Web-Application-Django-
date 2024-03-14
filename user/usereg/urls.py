56cf3dabee40cd5c0eec83d30157de9aab24ded7
unknown <antonydadu@gmail.com>
2024-03-14 11:44:21 -0400

    final registration web application

--- user/usereg/urls.py
+++ user/usereg/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('register',views.register, name="register"),
     path('login',views.login, name="login"),
     path('dashboard',views.dashboard, name="dashboard"),
+    path('user-logout', views.user_logout, name="user-logout"),
 
     
 ]
--- user/usereg/views.py
+++ user/usereg/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from django.http import HttpResponse
+from .forms import CreateUserForm, LoginForm
+
+from django.contrib.auth.decorators import login_required
+
+#- Authentication models and functions
+
+from django.contrib import auth
+from django.contrib.auth import authenticate, login, logout
 
 def homepage(request):
 
@@ -8,12 +15,58 @@ def homepage(request):
 
 def register(request):
 
-    return render(request, 'usereg/register.html')
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect("login")
+    
+    context = {'registerform':form}
+
+
+    return render(request, 'usereg/register.html', context= context)
+
+
 
 def login(request):
 
-    return render(request, 'usereg/login.html')
+    form = LoginForm()
 
+    if request.method == 'POST':
+
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password= password)
+
+            if user is not None:
+
+                auth.login(request, user)
+
+                return redirect("dashboard")
+            
+    context = {'loginform':form}
+
+
+    return render(request, 'usereg/login.html', context=context) 
+
+@login_required(login_url="login")
 def dashboard(request):
 
     return render(request, 'usereg/dashboard.html')
+
+
+def user_logout(request):
+
+    auth.logout(request) 
+
+    return redirect("")
